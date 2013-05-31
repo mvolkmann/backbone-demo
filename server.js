476de7c1ad0d7fd81a6856cf5828cab971c5efa1
Mark Volkmann <r.mark.volkmann@gmail.com>
2013-05-31 15:43:09 -0400

    reverted color; fixed assignment of port to match what Cloud 9 wants

--- server.js
+++ server.js
@@ -3,6 +3,5 @@ var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/public')); // serve static files
 
-var PORT = 3000;
-app.listen(PORT);
-console.log('Express server listening on port', PORT);
+app.listen(process.env.PORT);
+console.log('Express server listening on port', process.env.PORT);
